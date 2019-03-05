aab29dd5ca22c4c93c0eece684f205e3f0a7dcd1
Benjamin Vera <ben@soundcontrolrecords.com>
2019-03-05 17:13:56 -0600

    time to delete

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,62 +1,62 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'Shop',
+      pageTitle: 'All Products',
       path: '/products'
     });
   });
-}
+};
 
 exports.getProduct = (req, res, next) => {
-  const productId = req.params.productId;
-  Product.findById(productId, product => {
-    res.render('shop/product-detail', { 
-      product: product ,
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
       pageTitle: product.title,
       path: '/products'
-    })
-  })
-}
-
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
       prods: products,
-      pageTitle: 'All Products',
+      pageTitle: 'Shop',
       path: '/'
     });
   });
-}
+};
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
-    prods: [],
     path: '/cart',
     pageTitle: 'Your Cart'
-  })
-}
+  });
+};
 
 exports.postCart = (req, res, next) => {
-  const productId = req.body.productId;
-  
-}
-
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
-    prods: [],
     path: '/orders',
     pageTitle: 'Your Orders'
-  })
-}
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout'
-  })
-}
+  });
+};
--- models/product.js
+++ models/product.js
@@ -17,21 +17,33 @@ const getProductsFromFile = cb => {
   });
 };
 
-class Product {
-  constructor(title, imageUrl, price, description) {
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
-    this.price = price;
     this.description = description;
+    this.price = price;
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -41,10 +53,8 @@ class Product {
 
   static findById(id, cb) {
     getProductsFromFile(products => {
-      const product = products.find(p => p.id === id)
-      cb(product)
-    })
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
   }
 };
-
-module.exports = Product

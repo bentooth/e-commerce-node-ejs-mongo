2e0a4dc3f39526ce05fc3d819ebf8fca7cdca954
Benjamin Vera <ben@soundcontrolrecords.com>
2019-03-14 18:16:56 -0500

    learned a lot

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,54 @@
 const path = require('path');
+
 const express = require('express');
+const { body } = require('express-validator/check');
+
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
+
 const router = express.Router();
 
-router.get('/add-product', adminController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
-router.get('/products', adminController.getProducts);
+// /admin/products => GET
+router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product', adminController.postAddProduct);
+// /admin/add-product => POST
+router.post(
+  '/add-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post(
+  '/edit-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 module.exports = router;
